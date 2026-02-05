76fbfff7fa2e622e75545f041f852b92bf42f659
regginssa <randyarabis428@gmail.com>
2026-02-06 01:30:36 +0900

    feat: refine ChatContainer behavior and UI logic

--- components/organisms/chat/ChatContainer.tsx
+++ components/organisms/chat/ChatContainer.tsx
@@ -1,76 +1,37 @@
-import { ProfileDrawer } from "@/components/molecules";
-import { AntDesign } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
-import { useState } from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Footer, MainContainer } from "../layout";
+import { MainContainer } from "../layout";
 
 interface ChatContainerProps {
   children: React.ReactNode;
 }
 
 const ChatContainer: React.FC<ChatContainerProps> = ({ children }) => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-
   const router = useRouter();
 
   return (
     <MainContainer>
-      <SafeAreaView className="flex-1 px-5 gap-5">
-        <View className="w-full flex flex-row items-center justify-between sticky top-0">
+      <SafeAreaView className="flex-1 gap-5">
+        <View className="w-full px-5 flex flex-row items-center justify-between sticky top-0">
+          <TouchableOpacity
+            activeOpacity={0.8}
+            className="w-10 h-10 rounded-full bg-white flex flex-col items-center justify-center"
+            onPress={() => router.back()}
+          >
+            <Feather name="arrow-left" size={16} color="#4b5563" />
+          </TouchableOpacity>
           <Text className="font-poppins-semibold text-lg text-gray-800">
             Chat
           </Text>
-
-          <View className="flex flex-row items-center gap-2">
-            <TouchableOpacity
-              activeOpacity={0.8}
-              className="w-10 h-10 rounded-full bg-white flex items-center justify-center relative"
-              style={styles.bar}
-              onPress={() => setIsOpen(true)}
-            >
-              <AntDesign name="bell" size={20} color="#1f2937" />
-
-              <View className="absolute top-2 right-2 w-2 h-2 rounded-full bg-red-500"></View>
-            </TouchableOpacity>
-            <TouchableOpacity
-              activeOpacity={0.8}
-              className="w-10 h-10 rounded-full bg-white flex items-center justify-center relative"
-              style={styles.bar}
-              onPress={() => router.push("/chat" as any)}
-            >
-              <AntDesign name="message" size={20} color="#1f2937" />
-              <View className="absolute top-2 right-2 w-2 h-2 rounded-full bg-red-500"></View>
-            </TouchableOpacity>
-            <TouchableOpacity
-              activeOpacity={0.8}
-              className="w-10 h-10 rounded-full bg-white flex items-center justify-center"
-              style={styles.bar}
-              onPress={() => setIsOpen(true)}
-            >
-              <AntDesign name="bars" size={20} color="#1f2937" />
-            </TouchableOpacity>
-          </View>
+          <View className="w-10"></View>
         </View>
-        <View className="flex-1 gap-3">{children}</View>
+
+        <View className="flex-1">{children}</View>
       </SafeAreaView>
-
-      <ProfileDrawer isOpen={isOpen} onClose={() => setIsOpen(false)} />
-
-      <Footer />
     </MainContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  bar: {
-    shadowColor: "#000000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 6,
-    elevation: 3,
-  },
-});
 
 export default ChatContainer;
